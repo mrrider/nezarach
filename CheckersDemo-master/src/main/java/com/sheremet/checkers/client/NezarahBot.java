3c2429ba72aaeb80352e166094b5e48998fa5cfe
mrrider <cozak.nazar@gmail.com>
2016-12-16 11:07:51 +0200

    Add random

--- CheckersDemo-master/src/main/java/com/sheremet/checkers/client/NezarahBot.java
+++ CheckersDemo-master/src/main/java/com/sheremet/checkers/client/NezarahBot.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import checkers.client.CheckersBot;
@@ -34,27 +35,19 @@ public class NezarahBot implements CheckersBot{
         List<Step> hits = new ArrayList<>();
         List<Step> steps = new ArrayList<>();
 
-        // deep 3
-        prepareFirstSteps(board, steps, hits);
-//        prepareNextSteps(board, steps, hits, opposite(color), 1);
-//        prepareNextSteps(board, steps, hits, color, 2);
-//
-//        // deep 5
-//        prepareNextSteps(board, steps, hits, opposite(color), 3);
-//        prepareNextSteps(board, steps, hits, color, 4);
-
-        Step s = hits.isEmpty() ? steps.get(0) : hits.get(0);
+        prepareStep(board, steps, hits);
+        Step s;
+        if(!hits.isEmpty()){
+            int i = hits.size();
+            s = hits.get(new Random().nextInt(i));
+        }else{
+            int i = steps.size();
+            s = steps.get(new Random().nextInt(i));
+        }
         return s;
     }
 
-    private CheckerColor opposite(CheckerColor checkerColor) {
-        if (checkerColor == CheckerColor.BLACK)
-            return CheckerColor.WHITE;
-
-        return CheckerColor.BLACK;
-    }
-
-    private void prepareFirstSteps(final Board board, List<Step> steps, List<Step> hits) {
+    private void prepareStep(final Board board, List<Step> steps, List<Step> hits) {
         for (Checker checker : board.get(color)) {
             Position next;
             Position nextToNext;
@@ -90,168 +83,6 @@ public class NezarahBot implements CheckersBot{
             }
         }
     }
-
-//    private void prepareNextSteps(final Board original, List<StepSolution> steps, List<StepSolution> hits, CheckerColor checkerColor, int deep) {
-//        if (!hits.isEmpty()) {
-//            Set<StepSolution> toRemove = new HashSet<>();
-//            List<StepSolution> toAdd = new ArrayList<>();
-//            for (StepSolution solution : hits) {
-//                if (solution.size() < deep)
-//                    continue;
-//
-//                try {
-//                    Board clone = original.clone();
-//                    for (Step step : solution.getSteps()) {
-//                        clone.apply(step);
-//                    }
-//
-//                    Step last = solution.getSteps().get(solution.getSteps().size() - 1);
-//                    Checker checker = clone.get(last.getSteps().get(last.getSteps().size() - 1).getTo());
-//                    Position cp = checker.getPosition(), next, nextToNext;
-//
-//                    // up & right
-//                    next = new Position(cp.getX() + 1, cp.getY() + 1);
-//                    if (ValidationUtil.validPosition(next)) {
-//                        Checker checkerAtNext = clone.get(next);
-//                        nextToNext = new Position(next.getX() + 1, next.getY() + 1);
-//                        if (checkerAtNext != null && checkerAtNext.getColor() != checkerColor && ValidationUtil.validPosition(nextToNext) && clone.get(nextToNext) == null) {
-//                            StepSolution stepSolution = solution.clone();
-//                            stepSolution.addStep(new Step(Arrays.asList(new StepUnit(cp, nextToNext))));
-//                            toAdd.add(stepSolution);
-//                            toRemove.add(solution);
-//                        }
-//                    }
-//
-//                    // up & left
-//                    next = new Position(cp.getX() - 1, cp.getY() + 1);
-//                    if (ValidationUtil.validPosition(next)) {
-//                        Checker checkerAtNext = clone.get(next);
-//                        nextToNext = new Position(next.getX() - 1, next.getY() + 1);
-//                        if (checkerAtNext != null && checkerAtNext.getColor() != checkerColor && ValidationUtil.validPosition(nextToNext) && clone.get(nextToNext) == null) {
-//                            StepSolution stepSolution = solution.clone();
-//                            stepSolution.addStep(new Step(Arrays.asList(new StepUnit(cp, nextToNext))));
-//                            toAdd.add(stepSolution);
-//                            toRemove.add(solution);
-//                        }
-//                    }
-//
-//                    // down & left
-//                    next = new Position(cp.getX() - 1, cp.getY() - 1);
-//                    if (ValidationUtil.validPosition(next)) {
-//                        Checker checkerAtNext = clone.get(next);
-//                        nextToNext = new Position(next.getX() - 1, next.getY() - 1);
-//                        if (checkerAtNext != null && checkerAtNext.getColor() != checkerColor && ValidationUtil.validPosition(nextToNext) && clone.get(nextToNext) == null) {
-//                            StepSolution stepSolution = solution.clone();
-//                            stepSolution.addStep(new Step(Arrays.asList(new StepUnit(cp, nextToNext))));
-//                            toAdd.add(stepSolution);
-//                            toRemove.add(solution);
-//                        }
-//                    }
-//
-//                    // down & right
-//                    next = new Position(cp.getX() + 1, cp.getY() - 1);
-//                    if (ValidationUtil.validPosition(next)) {
-//                        Checker checkerAtNext = clone.get(next);
-//                        nextToNext = new Position(next.getX() + 1, next.getY() - 1);
-//                        if (checkerAtNext != null && checkerAtNext.getColor() != checkerColor && ValidationUtil.validPosition(nextToNext) && clone.get(nextToNext) == null) {
-//                            StepSolution stepSolution = solution.clone();
-//                            stepSolution.addStep(new Step(Arrays.asList(new StepUnit(cp, nextToNext))));
-//                            toAdd.add(stepSolution);
-//                            toRemove.add(solution);
-//                        }
-//                    }
-//                } catch (Exception e) {
-//
-//                }
-//            }
-//
-//            hits.removeAll(toRemove);
-//            hits.addAll(toAdd);
-//        } else if(!steps.isEmpty()) {
-//            Set<StepSolution> toRemove = new HashSet<>();
-//            List<StepSolution> toAdd = new ArrayList<>();
-//            for (StepSolution solution : steps) {
-//                if (solution.size() < deep)
-//                    continue;
-//
-//                try {
-//                    Board clone = original.clone();
-//                    for (Step step : solution.getSteps()) {
-//                        clone.apply(step);
-//                    }
-//
-//                    Step last = solution.getSteps().get(solution.getSteps().size() - 1);
-//                    Checker checker = clone.get(last.getSteps().get(last.getSteps().size() - 1).getTo());
-//                    Position cp = checker.getPosition(), next;
-//
-//                    // up & right
-//                    next = new Position(cp.getX() + 1, cp.getY() + 1);
-//                    if (ValidationUtil.validPosition(next)) {
-//                        try {
-//                            Step step = new Step(Arrays.asList(new StepUnit(cp, next)));
-//                            clone.clone().apply(step);
-//                            StepSolution stepSolution = solution.clone();
-//                            stepSolution.addStep(step);
-//                            toAdd.add(stepSolution);
-//                            toRemove.add(solution);
-//                        } catch (Exception e) {
-//
-//                        }
-//                    }
-//
-//                    // up & left
-//                    next = new Position(cp.getX() - 1, cp.getY() + 1);
-//                    if (ValidationUtil.validPosition(next)) {
-//                        try {
-//                            Step step = new Step(Arrays.asList(new StepUnit(cp, next)));
-//                            clone.clone().apply(step);
-//                            StepSolution stepSolution = solution.clone();
-//                            stepSolution.addStep(step);
-//                            toAdd.add(stepSolution);
-//                            toRemove.add(solution);
-//                        } catch (Exception e) {
-//
-//                        }
-//                    }
-//
-//                    // down & left
-//                    next = new Position(cp.getX() - 1, cp.getY() - 1);
-//                    if (ValidationUtil.validPosition(next)) {
-//                        try {
-//                            Step step = new Step(Arrays.asList(new StepUnit(cp, next)));
-//                            clone.clone().apply(step);
-//                            StepSolution stepSolution = solution.clone();
-//                            stepSolution.addStep(step);
-//                            toAdd.add(stepSolution);
-//                            toRemove.add(solution);
-//                        } catch (Exception e) {
-//
-//                        }
-//                    }
-//
-//                    // down & right
-//                    next = new Position(cp.getX() + 1, cp.getY() - 1);
-//                    if (ValidationUtil.validPosition(next)) {
-//                        try {
-//                            Step step = new Step(Arrays.asList(new StepUnit(cp, next)));
-//                            clone.clone().apply(step);
-//                            StepSolution stepSolution = solution.clone();
-//                            stepSolution.addStep(step);
-//                            toAdd.add(stepSolution);
-//                            toRemove.add(solution);
-//                        } catch (Exception e) {
-//
-//                        }
-//                    }
-//                } catch (Exception e) {
-//
-//                }
-//            }
-//
-//            steps.removeAll(toRemove);
-//            steps.addAll(toAdd);
-//        }
-//    }
 
     private void addStepForChecked(final Board board, Checker checker, Position next, Position nextToNext, List<Step> steps, List<Step> hits, CheckerColor checkerColor) {
         Checker c = board.get(next);
